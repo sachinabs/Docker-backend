2671c031ab242853bfd0ad300e540a49bb4e49ac
sachinabs <sachinabs.js@gmail.com>
2022-03-14 00:27:12 +0530

    feat:yml file updated

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send(`<h2>Hi There port is ${process.env.PORT}</h2>`);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
